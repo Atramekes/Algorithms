f2bb4979b58d2487612f8794ad429a19e72c98ba
Kortez <595976736@qq.com>
2019-08-09 23:54:03 +0800

    finish fibo

--- Dynamic_Programming/Fibonacci.py
+++ Dynamic_Programming/Fibonacci.py
@@ -6,19 +6,6 @@
 import sys
 import test_Dynamic_Programming
 
-def fast_Fibo(n):
-    if n == 1:
-        return 0, 1
-    m = n // 2
-    hprv, hcur = fast_Fibo(m)
-    prev = hprv * hprv + hcur * hcur
-    curr = hcur * (2 * hprv + hcur)
-    next = prev + curr
-    if n % 2 == 0:
-        return prev, curr
-    else:
-        return curr, next
-
 def Fibonacci(n):
     """ Solve Fibonacci problem.
         @params n (int): the input of the problem
@@ -26,32 +13,12 @@ def Fibonacci(n):
     """
 
     ### TODO - finish the function to compute the n-th fibonacci number
-    ###     use move(src, dst) to move disks
     ### YOUR CODE HERE
-    if n<=1:
-        return 1
-
-    # version 1: fastest
-    _, ans = fast_Fibo(n+1)
-    return ans
-
-    ''' version 2: remember everything
-    a = (n+1)*[0]
-    a[0] = 1
-    a[1] = 1
-    for i in range (n-1):
-        a[i+2]=a[i+1]+a[i]
-    return a[-1]
-    '''
-
-    ''' version 3: simple recursion
-    return Fibonacci(n-1) + Fibonacci(n-2)
-    '''
+    
     ### END YOUR CODE
 
 def run():
     ### finish this function to see your outputs (optional)
-    print(Fibonacci(4))
     return
 
 if __name__ == '__main__':
